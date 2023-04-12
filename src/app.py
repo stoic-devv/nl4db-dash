3b16f8b61092d0bb1e813b816e2489cc0ac315b8
Aditya <adityakanthale@gmail.com>
2023-04-11 20:21:14 -0500

    upload-parse flow

--- src/app.py
+++ src/app.py
@@ -7,27 +7,26 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output, State
 
-import pandas as pd
+import logging
 
-import dash_components as drc
+import utils.dash_components as drc
+from utils.process_sql import create_app_db, del_app_db, execute_sql_stream, get_tables_from_app_db
 
-
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     drc.UploadText(id='upload-data'),
-    drc.html_div(id='output-data-upload'),
+    #drc.html_div(id='output-data-upload'),
     
     html.Div([
         html.Label('Select a table:'),
         dcc.Dropdown(
-            id='table-selector',
-            options=[{'label': 'Table 1', 'value': 'table1'},
-                     {'label': 'Table 2', 'value': 'table2'},
-                     {'label': 'Table 3', 'value': 'table3'}],
-            value=None,
-            disabled=True
-        ),
+                id="table-selector",
+                # options=[{"label": i, "value": i} for i in admit_list],
+                # value=admit_list[:],
+                multi=True,
+            ),
         html.Label('Enter a sensitive attribute:'),
         dcc.Input(
             id='attribute-input',
@@ -38,7 +37,8 @@ app.layout = html.Div([
         html.Button('Submit', id='submit-button', disabled=True)
     ], style={'display': 'inline-block', 'margin': '10px'}),
     html.Div(id='output-form'),
-    html.Div(id='loading-output-1')
+    html.Div(id='loading-output-1'),
+    dcc.Store(id='sql_processing_state')
 ])
 
 def parse_contents(contents, filename):
@@ -49,63 +49,62 @@ def parse_contents(contents, filename):
     try:
         if 'sql' in filename:
             # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(content_string))
-        elif 'sql' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-        print(df.head())
-        return df
+            execute_sql_stream(decoded)
+        else:
+            app.logger.error('Invalid file format')
+            return html.Div([
+            'Invalid file format'
+        ])
 
     except Exception as e:
-        print('Error: ' + e)
+        app.logger.error(e)
         return html.Div([
             'There was an error processing this file.'
         ])
 
-# @app.callback(Output('output-data-upload', 'children'),
-#               Input('upload-data', 'contents'),
-#               State('upload-data', 'filename'))
-# def update_output(contents, filename):
-#     if contents is not None:
-#         # Simulate file processing
-#         df = parse_contents(contents, filename)
-#         return html.Div([
-#             html.Div('File "{}" has been uploaded and processed.'.format(filename)),
-#             dcc.Loading(id="loading-1", children=[html.Div(id='loading-output-1')], type="default"),
-#         ])
-#     else:
-#         return html.Div('Please upload a file.')
+@app.callback(Output('table-selector', 'options'),
+              Output('table-selector', 'disabled'),
+              Input('sql_processing_state', 'data'))
+def populate_tables_selector(is_processed):
+    tables = [{'label': 'x', 'value': 'y'}]
+    if is_processed:
+        tables = get_tables_from_app_db()
+        return [{'label': x, 'value':x} for x in tables], False
+    print(is_processed)
+    return tables, True
 
-@app.callback(Output('output-data-upload', 'children'),
+# uploads file and execute sql
+@app.callback(Output('loading-output-1', 'children'),
+              Output('sql_processing_state', 'data'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
 def update_output(list_of_contents, list_of_names, list_of_dates):
+    # backend processing
     if list_of_contents is not None:
         children = [
             parse_contents(list_of_contents, list_of_names)]
-        return children
+        return children, True
+    return [], False
 
-@app.callback(Output('loading-output-1', 'children'),
-              Input('upload-data', 'contents'))
-def update_output(contents):
-    if contents is not None:
-        # Simulate backend processing
-        time.sleep(5)
-        return html.Div('Processing completed.')
-    else:
-        return html.Div('')
+# # 
+# @app.callback(Output('output-data-upload', 'children'),
+#               Input('upload-data', 'contents'))
+# def update_output(contents):
+#     if contents is not None:
 
-@app.callback(Output('table-selector', 'disabled'),
-              Output('attribute-input', 'disabled'),
+#         return html.Div('Processing completed.')
+#     else:
+#         return html.Div('')
+
+@app.callback(Output('attribute-input', 'disabled'),
               Output('submit-button', 'disabled'),
-              Input('loading-output-1', 'children'))
-def enable_form(loading_output):
-    if loading_output == 'Processing completed.':
-        return True, True, True
+              Input('sql_processing_state', 'data'))
+def enable_form(is_processed):
+    if is_processed:
+        return False, False
     else:
-        return True, True, True
+        return True, True
 
 @app.callback(Output('output-form', 'children'),
               Input('submit-button', 'n_clicks'),
@@ -114,10 +113,19 @@ def enable_form(loading_output):
 def submit_form(n_clicks, table_name, attribute_name):
     if n_clicks is not None:
         # Placeholder function to simulate form submission
-        time.sleep(2)
         return html.Div('Form submitted with table "{}" and attribute "{}".'.format(table_name, attribute_name))
     else:
         return html.Div('')
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
+
+    try:
+        # ToDo: abstract init
+        create_app_db()
+        # run app server
+        app.run_server(debug=True, port=8050)
+    except Exception as e:
+        # handle the exception
+        print(e)
+    finally:
+        del_app_db()
