848e4bfc66f6fae5ddc0d9ff649460e365642247
Aditya <adityakanthale@gmail.com>
2023-04-13 14:53:42 -0500

    dataframe sql integration

---
+++ src/utils/pandas_sql.py
@@ -0,0 +1,17 @@
+import psycopg2
+import pandas as pd
+
+from .process_sql import _get_app_db_conn_
+
+
+def get_df_from_tablename(tablename):
+
+    # Connect to Postgres database
+    conn = _get_app_db_conn_()
+    # Query the database
+    query = f"SELECT * FROM {tablename};"
+    df = pd.read_sql(query, conn)
+
+    # Close the database connection
+    conn.close()
+    return df
